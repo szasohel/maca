afb164f515d0b4a2027ea81ec16be239ee429525
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2020-02-22 10:11:59 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'maca';
+export class AppComponent implements OnInit {
+  data;
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.http
+      .get(`http://api.aladhan.com/v1/calendar?latitude=39.8008&longitude=-74.9150&method=1&month=2&year=2020&timezonestring=America/New_York&latitudeAdjustmentMethod=3`)
+      .subscribe((data: any) => {
+        this.data = data.data[0].timings;
+      });
+  }
+
 }
