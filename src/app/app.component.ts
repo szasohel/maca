229ce327b4693ea07d3a96bd78adbbb6123e3e84
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2020-02-28 08:56:31 -0500

    add audio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,75 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  data = {
-    code: 200,
-    status: 'OK',
-    data: {
-      timings: {
-        Fajr: '05:28',
-        Sunrise: '06:42',
-        Dhuhr: '12:13',
-        Asr: '15:16',
-        Sunset: '17:45',
-        Maghrib: '17:45',
-        Isha: '18:59',
-        Imsak: '05:18',
-        Midnight: '00:13'
-      },
-      date: {
-        readable: '27 Feb 2020',
-        timestamp: '1582501552',
-        hijri: {
-          date: '28-06-1441',
-          format: 'DD-MM-YYYY',
-          day: '28',
-          weekday: { en: 'Al Ahad', ar: '\u0627\u0644\u0627\u062d\u062f' },
-          month: {
-            number: 6,
-            en: 'Jum\u0101d\u00e1 al-\u0101khirah',
-            ar:
-              '\u062c\u064f\u0645\u0627\u062f\u0649 \u0627\u0644\u0622\u062e\u0631\u0629'
-          },
-          year: '1441',
-          designation: { abbreviated: 'AH', expanded: 'Anno Hegirae' },
-          holidays: []
-        },
-        gregorian: {
-          date: '23-02-2020',
-          format: 'DD-MM-YYYY',
-          day: '23',
-          weekday: { en: 'Sunday' },
-          month: { number: 2, en: 'February' },
-          year: '2020',
-          designation: { abbreviated: 'AD', expanded: 'Anno Domini' }
-        }
-      },
-      meta: {
-        latitude: 39.8008,
-        longitude: -74.915,
-        timezone: 'America/New_York',
-        method: {
-          id: 2,
-          name: 'Islamic Society of North America (ISNA)',
-          params: { Fajr: 15, Isha: 15 }
-        },
-        latitudeAdjustmentMethod: 'ANGLE_BASED',
-        midnightMode: 'STANDARD',
-        school: 'STANDARD',
-        offset: {
-          Imsak: 0,
-          Fajr: 0,
-          Sunrise: 0,
-          Dhuhr: 0,
-          Asr: 0,
-          Maghrib: 0,
-          Sunset: 0,
-          Isha: 0,
-          Midnight: 0
-        }
-      }
-    }
-  };
+  data;
   date = new Date();
   mobile = false;
   days: number;
@@ -93,15 +25,14 @@ export class AppComponent implements OnInit {
     this.http
       .get(
         // tslint:disable-next-line: max-line-length
-        // `https://api.aladhan.com/v1/timings?latitude=39.8008&longitude=-74.9150&method=6&month=2&year=2020&timezonestring=America/New_York&latitudeAdjustmentMethod=3&date_or_timestamp=${this.today()}`
-        ''
+        `https://api.aladhan.com/v1/timings?latitude=39.8008&longitude=-74.9150&method=6&month=2&year=2020&timezonestring=America/New_York&latitudeAdjustmentMethod=3&date_or_timestamp=${this.today()}`
+
       )
       .subscribe((data: any) => {
         this.data = data;
         this.thtoday(data);
 
       });
-    this.thtoday(this.data);
   }
 
   playAudio() {
@@ -168,9 +99,13 @@ export class AppComponent implements OnInit {
       console.log(timeNow);
 
       // If the count down is over, write some text
-      if (distance === 0) {
+      if (distance <= 0) {
         this.playAudio();
-        this.itsPrayerTime = 'this is time for ' + this.nextPrayer;
+        clearInterval(x);
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
       }
     }, 1000);
   }
