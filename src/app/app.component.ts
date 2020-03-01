d0be8b57260919f0ea69fdd9c3c69dbc3280f4da
Sayed Zahed Abdullah Sohel <szasohel@gmail.com>
2020-02-29 19:37:55 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,7 @@ export class AppComponent implements OnInit {
     const maghrib = this.data.data.timings.Maghrib.replace(':', '');
     const isha = this.data.data.timings.Isha.replace(':', '');
     let countdown = '';
-    if (timeNow < fazr && timeNow > isha) {
+    if (timeNow < fazr || timeNow > isha) {
       countdown = this.data.data.timings.Fajr + ':00';
       this.nextPrayer = 'Fajr';
     } else if (timeNow > fazr && timeNow < dhuhr) {
