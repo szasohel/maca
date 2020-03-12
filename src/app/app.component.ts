c318662eccef2fb76428e9a97020ee72cb8efe29
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2020-03-12 17:11:35 -0400

    update tomorrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   data;
-  date = new Date();
+  dateToday = new Date();
+  dateTomorrow: Date;
   mobile = false;
   days: number;
   hours: number;
@@ -159,37 +160,59 @@ export class AppComponent implements OnInit {
       Isha: '20:30'
     }
   ];
+  apiData: any;
+  dateSet: string | (() => string);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   ngOnInit() {
     if (window.screen.availWidth < 600) {
       this.mobile = true;
     }
-    console.log(window.screen.availHeight, window.screen.availWidth);
+    this.getData();
+
+  }
+
+  onClick() {
+    this.dateSet = (this.dateSet === this.tomorrow()) ? this.today() : this.tomorrow();
+    this.setData(this.dateSet, this.apiData.data);
+  }
+
+  getData() {
     this.http
       .get(
         // tslint:disable-next-line: max-line-length
-        `https://api.aladhan.com/v1/timings?latitude=39.8008&longitude=-74.9150&method=6&month=2&year=2020&timezonestring=America/New_York&latitudeAdjustmentMethod=3&date_or_timestamp=${this.today()}`
+        `https://api.aladhan.com/v1/calendar?latitude=39.8008&longitude=-74.9150&method=6&month=${this.dateToday.getMonth() + 1}&year=2020&timezonestring=America/New_York&latitudeAdjustmentMethod=3`
       )
       .subscribe((data: any) => {
-        this.data = data;
+        this.apiData = data;
+        this.setData(this.today(), data.data);
+
         this.prayerTimeToday = this.getEqamat()[0];
         this.prayerTimeToday.Maghrib =
-          this.data.data.timings.Maghrib.substring(0, 3) +
-          (+this.data.data.timings.Maghrib.substr(3) + 5);
+          this.data.timings.Maghrib.substring(0, 3) +
+          (+this.data.timings.Maghrib.substr(3, 2) + 5);
+        console.log(this.data.timings.Maghrib.substr(3, 2) + 5);
+
         this.thtoday(data);
       });
   }
 
+  setData(day: string, data: any[]) {
+
+    this.data = data.filter((dayData) => {
+
+      return dayData.date.gregorian.date === day;
+    })[0];
+  }
+
   getEqamat() {
     return this.eqamatTime.filter((data, i) => {
-      const today = new Date(this.data.data.date.readable).getTime();
+      const today = new Date(this.data.date.readable).getTime();
       const prev = new Date(data.date).getTime();
       const next =
         i + 1 < this.eqamatTime.length
           ? new Date(this.eqamatTime[i + 1].date).getTime()
           : 0;
-      console.log(prev <= today, next);
 
       return prev <= today && today < next;
     });
@@ -204,51 +227,63 @@ export class AppComponent implements OnInit {
 
   today() {
     const date =
-      this.date.getDate() < 10
-        ? `0${this.date.getDate()}`
-        : `${this.date.getDate()}`;
-    const m = this.date.getMonth() + 1;
+      this.dateToday.getDate() < 10
+        ? `0${this.dateToday.getDate()}`
+        : `${this.dateToday.getDate()}`;
+    const m = this.dateToday.getMonth() + 1;
     const month = m < 10 ? `0${m}` : `${m}`;
-    return `${date}/${month}/${this.date.getFullYear()}`;
+    return `${date}-${month}-${this.dateToday.getFullYear()}`;
+  }
+
+  tomorrow() {
+    this.dateTomorrow = new Date(this.dateToday);
+    this.dateTomorrow.setDate(this.dateTomorrow.getDate() + 1);
+    const date =
+      this.dateTomorrow.getDate() < 10
+        ? `0${this.dateTomorrow.getDate()}`
+        : `${this.dateTomorrow.getDate()}`;
+    const m = this.dateTomorrow.getMonth() + 1;
+    const month = m < 10 ? `0${m}` : `${m}`;
+    return `${date}-${month}-${this.dateTomorrow.getFullYear()}`;
   }
 
   thtoday(data) {
     const formatedDate =
-      this.data.data.date.readable.slice(0, 6) +
+      this.data.date.readable.slice(0, 6) +
       ', ' +
-      this.data.data.date.readable.slice(7);
+      this.data.date.readable.slice(7);
     const timeNow = new Date()
       .toTimeString()
       .slice(0, 5)
       .replace(':', '');
-    const fazr = this.data.data.timings.Fajr.replace(':', '');
-    const dhuhr = this.data.data.timings.Dhuhr.replace(':', '');
-    const asr = this.data.data.timings.Asr.replace(':', '');
-    const maghrib = this.data.data.timings.Maghrib.replace(':', '');
-    const isha = this.data.data.timings.Isha.replace(':', '');
+    const fazr = this.data.timings.Fajr.replace(':', '');
+    const dhuhr = this.data.timings.Dhuhr.replace(':', '');
+    const asr = this.data.timings.Asr.replace(':', '');
+    const maghrib = this.data.timings.Maghrib.replace(':', '');
+    const isha = this.data.timings.Isha.replace(':', '');
     let countdown = '';
     if (timeNow < fazr) {
-      countdown = this.data.data.timings.Fajr + ':00';
+      countdown = this.data.timings.Fajr + ':00';
       this.nextPrayer = 'Fajr';
       localStorage.setItem('nextPrayer', this.nextPrayer);
     } else if (timeNow > fazr && timeNow < dhuhr) {
-      countdown = this.data.data.timings.Dhuhr + ':00';
+      countdown = this.data.timings.Dhuhr + ':00';
       this.nextPrayer = 'Dhuhr';
       localStorage.setItem('nextPrayer', this.nextPrayer);
     } else if (timeNow > dhuhr && timeNow < asr) {
-      countdown = this.data.data.timings.Asr + ':00';
+      countdown = this.data.timings.Asr + ':00';
       this.nextPrayer = 'Asr';
       localStorage.setItem('nextPrayer', this.nextPrayer);
     } else if (timeNow > asr && timeNow < maghrib) {
-      countdown = this.data.data.timings.Maghrib + ':00';
+      countdown = this.data.timings.Maghrib + ':00';
       this.nextPrayer = 'Maghrib';
       localStorage.setItem('nextPrayer', this.nextPrayer);
     } else if (timeNow > maghrib && timeNow < isha) {
-      countdown = this.data.data.timings.Isha + ':00';
+      countdown = this.data.timings.Isha + ':00';
       this.nextPrayer = 'Isha';
       localStorage.setItem('nextPrayer', this.nextPrayer);
-    } else if (timeNow > isha){
-      this.nextPrayer = 'Isha'
+    } else if (timeNow > isha) {
+      this.nextPrayer = 'Isha';
       localStorage.setItem('nextPrayer', this.nextPrayer);
     }
 
@@ -274,8 +309,8 @@ export class AppComponent implements OnInit {
 
       // If the count down is over, write some text
       if (distance <= 0) {
-        if (timeNow <= isha){
-        this.playAudio();
+        if (timeNow <= isha) {
+          this.playAudio();
         }
         clearInterval(x);
         this.days = 0;
--- src/app/pipes/timeformat.pipe.ts
+++ src/app/pipes/timeformat.pipe.ts
@@ -4,12 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'timeformat'
 })
 export class TimeformatPipe implements PipeTransform {
-  transform(time: any, ...args: unknown[]): unknown {
+  transform(time: string, ...args: unknown[]): unknown {
+    time = time.substring(0, 4);
     let hour = time.split(':')[0];
     let min = time.split(':')[1];
-    const part = hour > 12 ? 'pm' : 'am';
+    const part = +hour > 12 ? 'pm' : 'am';
     min = (min + '').length === 1 ? `0${min}` : min;
-    hour = hour > 12 ? hour - 12 : hour;
+    hour = +hour > 12 ? (+hour - 12).toString() : hour;
     hour = (hour + '').length === 1 ? `0${hour}` : hour;
     return `${hour}:${min} ${part}`;
   }
