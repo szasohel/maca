623101e44dda3d5e5d319530aa2e66daa1a33108
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2020-03-13 20:17:02 -0400

    add separate eqamattime file

---
+++ src/app/eqamatTime.ts
@@ -0,0 +1,142 @@
+export const times = [
+    {
+        date: '8 Mar 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Mar 2020',
+        Fajr: '06:15',
+        Dhuhr: '13:30',
+        Asr: '17:00',
+        Isha: '20:45'
+    },
+    {
+        date: '1 Apr 2020',
+        Fajr: '05:45',
+        Dhuhr: '13:30',
+        Asr: '17:00',
+        Isha: '21:00'
+    },
+    {
+        date: '16 Apr 2020',
+        Fajr: '05:30',
+        Dhuhr: '13:30',
+        Asr: '17:00',
+        Isha: '20:15'
+    },
+    {
+        date: '1 May 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 May 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Jun 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Jun 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Jul 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Jul 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Aug 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Aug 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Sep 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Sep 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Oct 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Oct 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Nov 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Nov 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '1 Dec 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    },
+    {
+        date: '16 Dec 2020',
+        Fajr: '06:30',
+        Dhuhr: '13:30',
+        Asr: '16:45',
+        Isha: '20:30'
+    }
+];
